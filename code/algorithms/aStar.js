99ee1c6baa7ad0f591c86a9d42d71310dfe80ea9
Yansarr <yanisgirardin@gmail.com>
2024-12-02 20:54:36 +0100

    AStar : generate start and goal cells

--- code/algorithms/aStar.js
+++ code/algorithms/aStar.js
@@ -3,9 +3,14 @@ export function aStar(maze) {
     const randomRowStart = Math.floor(Math.random() * maze.length);
     let randomColStart = Math.floor(Math.random() * maze[0].length);
 
-    let randomRowEnd
+    let randomRowEnd, randomColEnd
 
     if (randomRowStart > 0 || randomRowStart < maze.length - 1) {
       randomColStart = randomColStart <= 0.5 ? 0 : maze[0].length - 1;
+      randomColEnd = randomColStart == 0 ? maze[0].length - 1 : 0;
+      randomRowEnd = Math.floor(Math.random() * maze.length);
+    } else {
+      randomRowEnd = randomRowStart == 0 ? maze.length - 1 : 0;
+      randomColEnd = Math.floor(Math.random() * maze[0].length);
     }
 }
