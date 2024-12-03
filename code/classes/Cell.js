9a64759e7fb428e9f274216a4c528911860178a9
Noé <noepeutot@gmail.com>
2024-12-03 20:03:43 +0100

    - adapt dfs with maze walls - adapt maze.js & index.js & Cell.js

--- code/classes/Cell.js
+++ code/classes/Cell.js
@@ -4,7 +4,8 @@ export class Cell {
   x;
   y;
   walls;
-  visited;
+  visited; // Booléen indiquant si la cellule a été visitée (utilisé pour l'algorithme de génération de labyrinthe)
+  type; // Type de la cellule (start, end, normal)
 
   constructor(x, y, value = 0) {
     this.value = value;
@@ -12,18 +13,25 @@ export class Cell {
     this.y = y;
     this.walls = { top: true, right: true, bottom: true, left: true };
     this.visited = false;
+    this.setType("normal");
+  }
+
+  setType(type) {
+    if (type === "start") {
+      this.type = "start";
+    } else if (type === "end") {
+      this.type = "end";
+    } else {
+      this.type = "normal";
+    }
+  }
+
+  setVisited() {
+    this.visited = true;
   }
 
   isQueued() {
     return this.queued;
-  }
-
-  isEntry(entryCell) {
-    return this === entryCell;
-  }
-
-  isExit(exitCell) {
-    return this === exitCell;
   }
 }
 
